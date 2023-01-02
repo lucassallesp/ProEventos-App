fa59b7550a8eb31db0704d5cc60d313cb11f9f48
Lucas P. Salles <lucaspsalles@gmail.com>
2023-01-02 15:59:51 -0300

    Seção 5: Angular, Interpolação, Diretiva e Binding

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,4 +1,5 @@
 import { HttpClient } from '@angular/common/http';
+import { INT_TYPE } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,7 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public events: any = [];
+  public filteredEvents: any = [];
+  widthValue: number = 150;
+  marginValue: number = 2;
+  isShowing: boolean = true;
+  private _listFilter: string = '';
+
+  public get listFilter(): string {
+    return this._listFilter;
+  }
+
+  public set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredEvents = this.listFilter ? this.filterEvents(this.listFilter) : this.events;
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -16,9 +31,28 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  filterEvents(filterBy: string): any {
+    filterBy = filterBy.toLocaleLowerCase();
+
+    return this.events.filter(
+      (event: any) =>
+        event.tema.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
+        event.local.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
+        event.lote.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
+        event.dataEvento.toLocaleLowerCase().indexOf(filterBy) !== -1
+    );
+  }
+
+  changeImage() {
+    this.isShowing = !this.isShowing;
+  }
+
   public getEventos() : void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.events = response;
+        this.filteredEvents = this.events;
+      },
       error => console.log(error)
     );
   }
---
+++ Front/ProEventos-App/src/app/nav/nav.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements OnInit {
+  isCollapsed: boolean = true;
+  constructor() { }
+
+  collapseNav() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+
+  ngOnInit() {
+  }
+}
